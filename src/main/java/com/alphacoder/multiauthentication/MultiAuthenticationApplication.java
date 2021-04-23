88715b4d67574609c328b00b5de4ac4cf4c4d035
Ramesh Singh <singh.ramesh.eee@gmail.com>
2021-04-23 14:17:23 -0400

    Initial Project setup.

---
+++ src/main/java/com/alphacoder/multiauthentication/MultiAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.alphacoder.multiauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiAuthenticationApplication.class, args);
+	}
+
+}
